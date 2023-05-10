88fd1a0e27a098c15d9091f06cef6b2c2f543176
Thalyson Wiker <thalison.wilker@gmail.com>
2023-05-10 20:11:03 -0300

    organização de pastas

---
+++ numpy/01_vetores.py
@@ -0,0 +1,25 @@
+import numpy as np
+minha_lista = [1,2,3]
+np.array(minha_lista)
+
+# arrange: cria uma sequencia de numeros
+np_arange = np.arange(0,10, 2)
+
+# zeros: cria uma matriz de zeros
+
+np_zeros = np.zeros(2)
+
+# matriz bidimencional
+np_zeros_bidimencial = np.zeros((2,3))
+
+#matriz identidade
+np_eye = np.eye(3)
+
+# linspace:  ??
+np_linspace = np.linspace(1, 10, 3)
+
+np_random = np.random.rand(4)
+
+#matriz 3 dimensões
+
+#array: arr.shape, arr.max() arr.min() arr.argmax() arr.argmin()
